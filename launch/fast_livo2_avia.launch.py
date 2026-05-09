8dd21ad66e0b052ab587e1b777ef464a5ecb43aa
cakerdsp <1743826979@qq.com>
2026-05-09 13:20:37 +0800

    5.9：解决rviz报错

--- launch/fast_livo2_avia.launch.py
+++ launch/fast_livo2_avia.launch.py
@@ -3,6 +3,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -17,6 +18,8 @@ def generate_launch_description():
     config_file = LaunchConfiguration("config_file")
     rviz_config = LaunchConfiguration("rviz_config")
     use_rviz = LaunchConfiguration("use_rviz")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_sim_time_value = ParameterValue(use_sim_time, value_type=bool)
 
     return LaunchDescription(
         [
@@ -35,18 +38,24 @@ def generate_launch_description():
                 default_value="true",
                 description="Whether to launch RViz.",
             ),
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="true",
+                description="Use /clock from rosbag playback.",
+            ),
             Node(
                 package="cake_slam",
                 executable="cake_slam_node",
                 name="cake_slam_fast_livo2_avia",
                 output="screen",
-                parameters=[{"config_file": config_file}],
+                parameters=[{"config_file": config_file, "use_sim_time": use_sim_time_value}],
             ),
             Node(
                 package="rviz2",
                 executable="rviz2",
                 name="rviz2",
                 arguments=["-d", rviz_config],
+                parameters=[{"use_sim_time": use_sim_time_value}],
                 output="screen",
                 condition=IfCondition(use_rviz),
             ),
